e99048730fac781c018512b9aa116dae8655e02e
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:16:19 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,20 +1,17 @@
 Package.describe({
   name: 'babrahams:editable-text-froala',
   summary: 'Add Froala editor to babrahams:editable-text',
-  version: '0.2.12',
+  version: '0.2.16',
   git: 'https://github.com/JackAdams/meteor-editable-text-froala'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
     
-  api.versionsFrom('1.0');
+  api.versionsFrom(['1.8.2', '2.3']);
   
-  api.use('babrahams:editable-text@0.9.12', 'client');
-  api.use('froala:editor-reactive@2.8.4', 'client');
-  api.use('templating', 'client');
-  api.use('blaze', 'client');
-  api.use('spacebars', 'client');
-  api.use('jquery', 'client');
+  api.use('babrahams:editable-text@0.9.17', 'client');
+  api.use('froala:editor-reactive@2.9.7', 'client');
+  api.use(['templating@1.3.2', 'blaze@2.3.4', 'spacebars@1.0.15', 'jquery@1.11.11'], 'client');
   api.use('underscore', 'client');
   api.use('reactive-var', 'client');
   
@@ -24,7 +21,7 @@ Package.onUse(function(api) {
 
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
   api.use('babrahams:editable-text-froala');
   api.addFiles('editable-text-froala-tests.js');
